5e95e036abc7dc4c32a24171d1636ff08a383f51
sejun98 <tmdfl980520@naver.com>
2023-02-06 23:58:02 +0900

    3day

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,19 +5,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> list1 = new ArrayList<>();
-        ArrayList<Integer> list2 = new ArrayList<>();
-        Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < 28; i++) {
-            int a = sc.nextInt();
-            list1.add(a);
+        class Solution {
+            public double solution(int[] arr) {
+                double answer = 0;
+                for (int i = 0; i < arr.length; i++) {
+                    answer += arr[i];
+                }
+                return answer/arr.length;
+            }
         }
-        for (int i = 1; i <= 30; i ++) {
-            list2.add(i);
-        }
-        list2.removeAll(list1);
-        for (int i = 0; i < 2; i++) {
-            System.out.println(list2.get(i));
-        }
+
     }
 }
---
+++ src/main/java/org/example/q5598.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class q5598 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 28; i++) {
+            int a = sc.nextInt();
+            list1.add(a);
+        }
+        for (int i = 1; i <= 30; i ++) {
+            list2.add(i);
+        }
+        list2.removeAll(list1);
+        for (int i = 0; i < 2; i++) {
+            System.out.println(list2.get(i));
+        }
+    }
+}
