ff881232b21821667468b6fae11e0ec7735b636d
sejun98 <tmdfl980520@naver.com>
2023-02-08 23:32:11 +0900

    5day

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        class Solution {
-            public double solution(int[] arr) {
-                double answer = 0;
-                for (int i = 0; i < arr.length; i++) {
-                    answer += arr[i];
-                }
-                return answer/arr.length;
-            }
-        }
 
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int b = n;
+        int a = 0;
+        while (n > 0) {
+            n = n / 10;
+            a++;
+        }
+        int[] answer =  new int[a];
+        System.out.println(a);
+        for(int i = 0; i < a; i++) {
+            answer[i] = b % 10;
+            b/=10;
+            System.out.println(answer[i]);
+        }
     }
 }
