5a042091c785f7d812f345b0b8a5b81bfd8b51da
sejun98 <tmdfl980520@naver.com>
2023-02-12 19:25:35 +0900

    8day

--- src/main/java/org/example/x만큼간격이있는n개의숫자.java
+++ src/main/java/org/example/x만큼간격이있는n개의숫자.java
@@ -1,10 +1,6 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Scanner;
-
-public class Main {
+public class x만큼간격이있는n개의숫자 {
     public static void main(String[] args) {
         int n = 100;
         int x = -12;
